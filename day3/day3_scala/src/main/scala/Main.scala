4d5e87f313fb69a9f81580137332956bfe221c5c
kraktus <56031107+kraktus@users.noreply.github.com>
2020-12-03 14:56:07 +0000

    Refractor code

--- day3/day3_scala/src/main/scala/Main.scala
+++ day3/day3_scala/src/main/scala/Main.scala
@@ -6,47 +6,37 @@ object Main extends App {
 
 	class Matrix(
 		private val matrixVec: Vector[String],
-		private val row: Int,
-		private val column: Int
+		val row: Int,
+		val column: Int
 	) {
 
-		def check_sanity: Boolean =
-			matrixVec.foldLeft(true)(
-				(b: Boolean, a: String) => b match {
-					case true => a.length == column
-					case _ => false
-					}
-			) && matrixVec.length == row
-
-		def getRow = row
-		def getColumn = column
-
-		def get(row_input: Int, column_input: Int): Char =
+		def get(rowInput: Int, columnInput: Int): Char =
 			// never get out of range
-			matrixVec(row_input % row).charAt(column_input % column)
+			matrixVec(rowInput % row).charAt(columnInput % column)
 
 	}
 
-	def createMatrix(lines: Vector[String]): Matrix = {
+	def createMatrix(lines: Vector[String]): Option[Matrix] = {
 
 		val matrixVec: Vector[String] = lines.map(
 			_.filter((x: Char) => x == '#' || x == '.')
 		)
 		val row: Int = matrixVec.length
-		val column: Int = matrixVec(0).length // unsafe for now
-		new Matrix(matrixVec, row, column)
+		val column: Int = matrixVec(0).length
+		if (matrixVec.foldLeft(true)((b: Boolean, a: String) => b && a.length == column) && matrixVec.length == row)
+			Some(new Matrix(matrixVec, row, column))
+		else {
+			None
+		}
 	}
 
 	def countTree(matrix: Matrix, xIncr: Int, yIncr: Int): Int = {
-		(0 until matrix.getRow by yIncr).map(
+		(0 until matrix.row by yIncr).map(
 			(y: Int) => (y, y/yIncr * xIncr) // y, x
 		)
-		.foldLeft(0)(
-			(acc: Int, coord: Tuple2[Int, Int]) => matrix.get(coord._1, coord._2) match {
-				case '#' => acc + 1
-				case _ => acc
-			}
-		)
+		.count {
+			case (y, x) => matrix.get(y, x) == '#'
+		}
 	}
 
 	def countTreeOnMultiplePath(matrix: Matrix): BigInt =
@@ -55,16 +45,16 @@ object Main extends App {
 		.map {
 			case (a, b) => countTree(matrix, a, b)
 		}
-		.map(BigInt(_))
+		.map(_.toLong)
 		.product
 
 	val mapInput = Source.fromFile("../input_day3.txt").getLines.toVector
 	// val mapInput = Source.fromFile("../small_input.txt").getLines.toList
-	val matrixToboggan = createMatrix(mapInput)
-	if (matrixToboggan.check_sanity) {
-		println(countTree(matrixToboggan, 3, 1)) // challenge 1
-		println(countTreeOnMultiplePath(matrixToboggan)) // challenge 2
-	} else {
-		println("matrix is not sane")
-	}
+	createMatrix(mapInput) match {
+		case Some(matrix) => {
+			println(countTree(matrix, 3, 1)) // challenge 1
+			println(countTreeOnMultiplePath(matrix)) // challenge 2
+			}
+		case _ => println("input is not sane")
+		}
 }
