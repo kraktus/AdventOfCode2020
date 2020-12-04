1a61f40dfeb1c09210e12ec159ad92de1a06e7d6
kraktus <56031107+kraktus@users.noreply.github.com>
2020-12-04 22:20:06 +0000

    Start refractoring code

--- day4/day4_scala/src/main/scala/Main.scala
+++ day4/day4_scala/src/main/scala/Main.scala
@@ -5,32 +5,25 @@ import scala.util.matching.Regex
 object Main extends App {
 
 	def splitByPassport(l: List[String], acc: String = ""): List[String] = l match {
-		case "" :: tail =>  acc :: splitByPassword(tail)
-		case head :: tail => splitByPassword(tail, acc + head)
+		case "" :: tail =>  acc :: splitByPassport(tail)
+		case head :: tail => splitByPassport(tail, acc + " " + head) //at least one space between two keys
 		case _ => List(acc)
 	}
 
 	def isPassport(potentialPassport: String): Boolean = 
-		Vector("byr".r, "iyr".r, "eyr".r, "hgt".r, "hcl".r, "ecl".r, "pid".r)
-		.forall((x: Regex) => (x findFirstIn potentialPassport).isDefined)
+		Vector("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
+		.forall(potentialPassport contains _)
 
-	def between(x: String, lowerBound: Int, upperBound: Int): Boolean = lowerBound <= x.toInt && x.toInt <= upperBound
+	def between(x: String, lowerBound: Int, upperBound: Int): Boolean = x.toIntOption.exists(n => lowerBound <= n && n <= upperBound)
 
 	def arePassportDatesOk(potentialPassport: String): Boolean = 
 		Vector(("byr:(\\d{4})".r, 1920, 2002), ("iyr:(\\d{4})".r, 2010, 2020), ("eyr:(\\d{4})".r, 2020, 2030)) // regex and lower + upper bound
-		.forall { case (rex, lowerBound, upperBound) => rex findFirstMatchIn potentialPassport match {
-			case Some(m) => between(m.group(1), lowerBound, upperBound)
-			case _ => false
-			}
+		.forall { case (rex, lowerBound, upperBound) => (rex findFirstMatchIn potentialPassport)
+		.exists((m: Regex.Match) => between(m.group(1), lowerBound, upperBound)) 
 		}
 
-	def isHeightOK(potentialPassport: String): Boolean = {
-		val height = "hgt:(\\d{2,3})(cm|in)".r
-		height findFirstMatchIn potentialPassport match {
-			case Some(m) => (m.group(2) == "cm" && between(m.group(1), 150, 193)) || (m.group(2) == "in" && between(m.group(1), 59, 76))
-			case _ => false
-		}
-	}
+	def isHeightOK(potentialPassport: String): Boolean =
+		("hgt:(\\d{2,3})(cm|in)".r findFirstMatchIn potentialPassport).exists((m: Regex.Match) => (m.group(2) == "cm" && between(m.group(1), 150, 193)) || (m.group(2) == "in" && between(m.group(1), 59, 76)))
 
 	def isTruePassport(potentialPassport: String): Boolean =
 		(Vector("hcl:#(\\d|[abcdef]){6}".r, "ecl:(amb|blu|brn|gry|grn|hzl|oth)".r, "pid:\\d{9}".r) // validity can be checked with regex only
