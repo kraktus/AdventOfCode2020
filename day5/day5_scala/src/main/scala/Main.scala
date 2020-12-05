da2e9fc882f715044b8184cc7ddff20ec67c71fe
kraktus <56031107+kraktus@users.noreply.github.com>
2020-12-05 12:37:29 +0000

    use space everywhere

--- day5/day5_scala/src/main/scala/Main.scala
+++ day5/day5_scala/src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import scala.util.matching.Regex
 
 object Main extends App {
 
-	case class Seat(row: Int, column: Int)
+    case class Seat(row: Int, column: Int)
 
     def readFile(line: List[Char]): Option[Seat] = {
         line.splitAt(7) match {
@@ -16,30 +16,30 @@ object Main extends App {
             case _ => None
         }
     }
-	// a classic
-	def dichotomy(input: List[Char], lowerBound: Int, upperBound: Int): Option[Int] = 
-		input match {
-			case ('R'|'B') :: Nil => Some(upperBound)
-			case ('F'|'L') :: Nil => Some(lowerBound)
-			case ('R'|'B') :: tail => dichotomy(tail, (lowerBound+upperBound)/2+1, upperBound)
-			case ('F'|'L') :: tail => dichotomy(tail, lowerBound, (lowerBound+upperBound)/2)
-			case _ => None
-		}
+    // a classic
+    def dichotomy(input: List[Char], lowerBound: Int, upperBound: Int): Option[Int] = 
+        input match {
+            case ('R'|'B') :: Nil => Some(upperBound)
+            case ('F'|'L') :: Nil => Some(lowerBound)
+            case ('R'|'B') :: tail => dichotomy(tail, (lowerBound+upperBound)/2+1, upperBound)
+            case ('F'|'L') :: tail => dichotomy(tail, lowerBound, (lowerBound+upperBound)/2)
+            case _ => None
+        }
 
-	def missingSeat(sortedSeatIds: List[Int]): Option[Int] = {
-		sortedSeatIds match {
-			case x :: y :: tail if y == x + 1 => missingSeat(y :: tail)
-			case x :: y :: _ if y == x + 2 => Some(x+1)
-			case _ => None
-		}
-	}
+    def missingSeat(sortedSeatIds: List[Int]): Option[Int] = {
+        sortedSeatIds match {
+            case x :: y :: tail if y == x + 1 => missingSeat(y :: tail)
+            case x :: y :: _ if y == x + 2 => Some(x+1)
+            case _ => None
+        }
+    }
 
-	val input = Source.fromFile("../input_day5.txt").getLines.toList
-	// can't use flatmap on List[List[Char]]
-	val seatList: List[Seat] = input.map(_.toList).map(readFile _).flatten
-	val seatIds: List[Int] = seatList.map {case Seat(y, x) => y*8 + x}
-	println(seatIds.max) // challenge 1
-	val sortedSeatIds = seatIds.sorted
-	println(missingSeat(sortedSeatIds)) // challenge 2
+    val input = Source.fromFile("../input_day5.txt").getLines.toList
+    // can't use flatmap on List[List[Char]]
+    val seatList: List[Seat] = input.map(_.toList).map(readFile _).flatten
+    val seatIds: List[Int] = seatList.map {case Seat(y, x) => y*8 + x}
+    println(seatIds.max) // challenge 1
+    val sortedSeatIds = seatIds.sorted
+    println(missingSeat(sortedSeatIds)) // challenge 2
 
 }
