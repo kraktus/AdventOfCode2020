e4488572d36bf6919a75b8a4884f0f2840028a1a
kraktus <56031107+kraktus@users.noreply.github.com>
2020-12-06 14:45:23 +0000

    refractor day6

--- day6/day6_scala/src/main/scala/Main.scala
+++ day6/day6_scala/src/main/scala/Main.scala
@@ -4,17 +4,17 @@ import scala.util.matching.Regex
 
 object Main extends App {
 
-	def dummyUnion(a: Set[Char], b: Set[Char]): Set[Char] = a union b
-	def dummyIntersection(a: Set[Char], b: Set[Char]): Set[Char] = a intersect b
+	def union[A](a: Set[A], b: Set[A]): Set[A] = a union b // calling the standard lib func
+	def intersect[A](a: Set[A], b: Set[A]): Set[A] = a intersect b // calling the standard lib func
 
 	def splitByPersonAndCount(l: List[String], acc: Set[Char], op: (Set[Char], Set[Char]) => Set[Char]): Int = l match {
 		case "" :: Nil => acc.size
-	 	case "" :: head :: tail => acc.size + splitByPerson(tail, head.toSet, op)
-	 	case head :: tail => splitByPerson(tail, op(acc,head.toSet), op)
+	 	case "" :: head :: tail => acc.size + splitByPersonAndCount(tail, head.toSet, op)
+	 	case head :: tail => splitByPersonAndCount(tail, op(acc,head.toSet), op)
 	 	case _ => acc.size
 	}
 
 	val questionsInput = Source.fromFile("../input_day6.txt").getLines.toList
-	println(splitByPersonAndCount(questionsInput.tail, questionsInput.head.toSet, dummyUnion)) //challenge 1
-	println(splitByPersonAndCount(questionsInput.tail, questionsInput.head.toSet, dummyIntersection)) //challenge 2
+	println(splitByPersonAndCount(questionsInput.tail, questionsInput.head.toSet, union)) //challenge 1
+	println(splitByPersonAndCount(questionsInput.tail, questionsInput.head.toSet, intersect)) //challenge 2
 }
